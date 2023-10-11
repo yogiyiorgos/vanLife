812106495d9a687264e5e930136ecfbe88fb395a
yogiyiorgos <yiorgosdev@gmail.com>
2023-10-11 22:49:29 +0200

    VL ChCondRend Oct11

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -32,6 +32,12 @@ const Vans = () => {
   return (
     <div className='van-list-container'>
       <h1>Explore our van options</h1>
+      <div className='van-list-filter-buttons'>
+        <button onClick={() => setSearchParams({type: 'simple'})} className='van-type simple'>Simple</button>
+        <button onClick={() => setSearchParams({type: 'luxury'})} className='van-type luxury'>Luxury</button>
+        <button onClick={() => setSearchParams({type: 'rugged'})} className='van-type rugged'>Rugged</button>
+        <button onClick={() => setSearchParams({type: ''})} className='van-type clear-filters'>Clear filter</button>
+      </div>
       <div className='van-list'>
         {vanElements}
       </div>
